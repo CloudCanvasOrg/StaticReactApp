9d82f0e8dea37c4f5ac9b6b95c4420df27cd77d8
Ethan Durward <rreeves8@uwo.ca>
2024-12-04 00:21:16 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
+import axios from "axios";
+import { useCallback, useState } from "react";
 
-function App() {
+export default function App() {
+  const [result, setResult] = useState();
+
+  const callLambda = useCallback(async () => {
+    const res = await axios.get("/api");
+    setResult(JSON.stringify(res.data));
+  }, []);
+
   return (
-    <div className="App">
-      <div>Cloud canvas testing</div>
-      <p>
-        static app example
-      </p>
+    <div className="m-10 flex flex-col gap-3">
+      <article className="prose">
+        <h1>Cloud Canvas </h1>
+        <p>Static react app, deployed on cloud front</p>
+      </article>
+      <div>
+        <button className="btn" onClick={callLambda}>
+          Call Lambda
+        </button>
+        <article className="prose">
+          <p>{result}</p>
+        </article>
+      </div>
     </div>
   );
 }
-
-export default App;
